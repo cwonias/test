99e1850e72697282deba5b6b09bd1f7c4e1a5e5d
cwonias <cwon@integralads.com>
2015-02-23 14:37:52 -0500

    first commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args){
+		System.out.println(TestEnum.Test1.values()[0]);
+	}
+}
---
+++ src/com/test/TestEnum.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public enum TestEnum {
+    Test1("t1"),
+    Test2("t2");
+    private String value;
+    
+    private TestEnum(String val){
+        this.value = val;	
+    }
+    
+    public String getValue(){
+    	return value;
+    }
+}
