2cf6b22ce748057b21fa1d69cbd5a2169110863c
cwonias <cwon@integralads.com>
2015-02-23 15:00:47 -0500

    Commit Branch_1

--- src/com/test/TestEnum.java
+++ src/com/test/TestEnum.java
@@ -7,13 +7,10 @@ public enum TestEnum {
     
     private TestEnum(String val){
         this.value = val;
-<<<<<<< HEAD
 		//Change in master 1.
 		//Change in master 2.
-=======
 		//Change in Branch_1 1.
 		//Change in Branch_1 2.
->>>>>>> commit testenum
     }
     
     public String getValue(){
